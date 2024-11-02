7ce1cf47f9041d2160a2088d762f2cf1fac22097
monca <monca@192.168.1.171>
2024-11-03 00:40:29 +0100

    commit SAX y DOM

--- src/dao/xml/SaxReader.java
+++ src/dao/xml/SaxReader.java
@@ -9,78 +9,68 @@ import org.xml.sax.helpers.DefaultHandler;
 import model.Amount;
 import model.Product;
 
-public class SaxReader extends DefaultHandler{
-	ArrayList <Product> products = new ArrayList();
+public class SaxReader extends DefaultHandler {
+	ArrayList<Product> products = new ArrayList();
 	Product product;
-	private StringBuilder buffer = new StringBuilder(); //para poder ir leyendo lso elementos simples (con texto)
+	StringBuilder buffer = new StringBuilder(); // para poder ir leyendo los elementos simples (con texto)
 
-	
-	//double price;
-	//int stock;
-	
-	
-	//método de acceso para leer el ArrayList desde el main:
-
-	public ArrayList<Product> getProducts(){
+	public ArrayList<Product> getProducts() {
 		return products;
 	}
-	
-	/*public void setProducts(ArrayList<Product> products) {
-		this.products= products;
-	}*/
-	
-	/*@Override
-	public void startDocument() throws SAXException{
-		this.products = new ArrayList <>();
-	}*/
-	
+
 	@Override
-	public void startElement(String uri,String localName, String qName, Attributes attributes) throws SAXException{
-		switch(qName) {
-		case "inventory":
-			break;
+	public void startDocument() throws SAXException {
+		this.products = new ArrayList<Product>();
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		switch (qName) {
 		case "product":
-			break;
-		case "name":
 			String productName = attributes.getValue("name");
 			product = new Product(productName, new Amount(0), true, 0);
 			products.add(product);
-			
-			
-			
-		
-			this.product = new Product();
-			break;
-		case "wholesalerPrice":
-			break;
-		case "stock":
-			break;
-		}
-	}
-	
-	
-	@Override
-	public void endElement(String uri,String localName, String qName) throws SAXException{
-		switch(qName) {
-		case "inventory":
-			break;
-		case "product":
+			// this.product = new Product();
 			break;
 		case "name":
-			break;
 		case "wholesalerPrice":
-			break;
 		case "stock":
+			buffer.delete(0, buffer.length()); // Limpiar buffer al inicio de cada elemento
 			break;
 		}
 	}
-	
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		switch (qName) {
+		case "name":
+			product.setName(buffer.toString());
+			break;
+		case "wholesalerPrice":
+			double priceValue = Double.parseDouble(buffer.toString());
+			product.setWholesalerPrice(new Amount(priceValue));
+			break;
+		case "stock":
+			product.setStock(Integer.parseInt(buffer.toString()));
+			break;
+		}
+	}
+
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
-		buffer.append(ch, start, length); //guardamos el texto en el buffer
-	} 
-	
-	
-	
+		buffer.append(ch, start, length); // Guardamos el texto en el buffer
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		printDocument();
+	}
+
+	private void printDocument() {
+		for (Product p : products) {
+			System.out.println(p.toString());
+		}
+
+	}
 
 }
