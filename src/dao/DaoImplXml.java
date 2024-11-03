7b7c633d4080c6a94d699f01a35f71bbba156dd4
monca <monca@192.168.1.130>
2024-11-03 18:25:11 +0100

    sec

---
+++ src/dao/DaoImplXml.java
@@ -0,0 +1,61 @@
+package dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+import dao.xml.DomWriter;
+import dao.xml.SaxReader;
+import model.Employee;
+import model.Product;
+
+public class DaoImplXml implements Dao {
+
+	@Override
+	public void connect() {
+
+	}
+
+	@Override
+	public void disconnect() {
+
+	}
+
+	@Override
+	public Employee getEmployee(int employeeId, String password) {
+		return null;
+	}
+
+	@Override
+	public ArrayList<Product> getInventory() {
+		SaxReader handler = null;
+		try {
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser parser;
+			parser = factory.newSAXParser();
+			File file = new File("files/inputInventory.xml");
+
+			handler = new SaxReader();
+			parser.parse(file, handler);
+		} catch (ParserConfigurationException | SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return handler.getProducts();
+	}
+
+	@Override
+	public boolean writeInventory(ArrayList<Product> inventory) {
+		DomWriter domWriter = new DomWriter(inventory);
+		return domWriter.generateDocument();
+	}
+
+}
---
+++ src/dao/xml/DomWriter.java
@@ -0,0 +1,116 @@
+package dao.xml;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.Source;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import model.Amount;
+import model.Product;
+
+public class DomWriter {
+	
+	private Document document;
+	
+	private ArrayList<Product> copyInventory;
+
+	public DomWriter(ArrayList<Product> inventory) {
+		try {
+			copyInventory = inventory;
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			document = builder.newDocument();
+		} catch (ParserConfigurationException e) {
+			System.out.println("Error generating document");
+		}
+	}
+	
+	public boolean generateDocument() {
+		//PAREND NODE
+		Element products = document.createElement("products");
+	    products.setAttribute("total", String.valueOf(copyInventory.size()));
+		document.appendChild(products);
+		
+		//CHILD NODES
+		for (Product productItem : copyInventory) {
+
+			Element product = document.createElement("product");
+	        product.setAttribute("id", String.valueOf(productItem.getId()));
+	        products.appendChild(product);
+	        
+	        Element name = document.createElement("name");
+	        name.setTextContent(productItem.getName());
+	        product.appendChild(name);
+	        
+	    /*    Element price = document.createElement("price");
+	        price.setAttribute("Currency", Amount.getValue());
+	        price.setTextContent(String.valueOf(productItem.getPublicPrice()));
+	        product.appendChild(price);
+	        */
+	        Element stock = document.createElement("stock");
+	        stock.setTextContent(String.valueOf(productItem.getStock()));
+	        product.appendChild(stock);
+	        
+	    }
+		
+		 if(generateXml()) {
+		    	return true;
+		    }else {
+		    	return false;
+		    }
+	    
+	}
+	
+	public boolean generateXml() {
+		boolean generated = false;
+
+		try {
+			String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+			String fileName = "inventory_" + date + ".xml";
+			
+			File directory =  new File("files");
+			if (!directory.exists()) {
+				directory.mkdirs();
+			}
+			
+			File file = new File("files/" + fileName);
+			FileWriter fw = new FileWriter(file);
+			PrintWriter pw = new PrintWriter(fw);
+			
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer = factory.newTransformer();
+			
+			Source source = new DOMSource(document);
+			Result result = new StreamResult(pw);
+			
+			transformer.transform(source, result);
+			System.out.println("XML document saved as: " + fileName);
+
+			
+		} catch(IOException e) {
+			System.out.println("Error when creating writter file");
+		} catch(TransformerException e) {
+			System.out.println("Error transforming the document");
+		}
+		return generated;
+
+	}
+}
